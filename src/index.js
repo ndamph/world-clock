2193a19f6cd039cbec87140787ea2c8f38494ba0
Nadine D <158780700+ndamph@users.noreply.github.com>
2024-04-08 14:54:59 -0500

    Created select functionality in JS file

--- src/index.js
+++ src/index.js
@@ -1,28 +1,55 @@
 function displayTime() {
   //Los Angeles
-  let losAngelesDateElement = document.querySelector(`#los-angeles-date`);
-  let losAngelesTimeElement = document.querySelector(`#los-angeles-time`);
-  let losAngelesDateFormat = moment()
-    .tz(`America/Los_Angeles`)
-    .format(`MMMM Do YYYY`);
-  let losAngelesTimeFormat = moment()
-    .tz(`America/Los_Angeles`)
-    .format(`h:mm:ss [<span>]A[</span>]`);
+  let losAngelesElement = document.querySelector(`#los-angeles`);
+  if (losAngelesElement) {
+    let losAngelesDateElement = document.querySelector(`#los-angeles-date`);
+    let losAngelesTimeElement = document.querySelector(`#los-angeles-time`);
+    let losAngelesDateFormat = moment()
+      .tz(`America/Los_Angeles`)
+      .format(`MMMM Do YYYY`);
+    let losAngelesTimeFormat = moment()
+      .tz(`America/Los_Angeles`)
+      .format(`h:mm:ss [<span>]A[</span>]`);
 
-  losAngelesDateElement.innerHTML = losAngelesDateFormat;
-  losAngelesTimeElement.innerHTML = losAngelesTimeFormat;
+    losAngelesDateElement.innerHTML = losAngelesDateFormat;
+    losAngelesTimeElement.innerHTML = losAngelesTimeFormat;
+  }
 
   //Paris
-  let parisDateElement = document.querySelector(`#paris-date`);
-  let parisTimeElement = document.querySelector(`#paris-time`);
-  let parisDateFormat = moment().tz(`Europe/Paris`).format(`MMMM Do YYYY`);
-  let parisTimeFormat = moment()
-    .tz(`Europe/Paris`)
-    .format(`h:mm:ss [<span>]A[</span>]`);
+  let parisElement = document.querySelector(`#paris`);
+  if (parisElement) {
+    let parisDateElement = document.querySelector(`#paris-date`);
+    let parisTimeElement = document.querySelector(`#paris-time`);
+    let parisDateFormat = moment().tz(`Europe/Paris`).format(`MMMM Do YYYY`);
+    let parisTimeFormat = moment()
+      .tz(`Europe/Paris`)
+      .format(`h:mm:ss [<span>]A[</span>]`);
 
-  parisDateElement.innerHTML = parisDateFormat;
-  parisTimeElement.innerHTML = parisTimeFormat;
+    parisDateElement.innerHTML = parisDateFormat;
+    parisTimeElement.innerHTML = parisTimeFormat;
+  }
 }
+
+function showSelectedCity(event) {
+  let selectedCity = event.target.value;
+  let citiesName = selectedCity.replace(`_`, ` `).split(`/`)[1];
+  let citiesElement = document.querySelector(`#cities`);
+  let citiesDate = moment().tz(selectedCity).format(`MMMM Do YYYY`);
+  let citiesTime = moment()
+    .tz(selectedCity)
+    .format(`h:mm:ss [<span>]A[</span>]`);
+  citiesElement.innerHTML = `
+          <div class="city">
+          <div>
+            <h2>${citiesName}</h2>
+            <div class="date">${citiesDate}</div>
+          </div>
+          <div class="time">${citiesTime}</div>
+        </div>
+  `;
+}
+let citySelectElement = document.querySelector(`#city-select`);
+citySelectElement.addEventListener(`change`, showSelectedCity);
 
 displayTime();
 setInterval(displayTime, 1000);
